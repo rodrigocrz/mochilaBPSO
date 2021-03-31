e0a90399a56dff534e9bda068eadf6bb3356ced0
Angel <angel_diaz_mera@outlook.com>
2021-03-31 07:21:14 -0600

    Condición final y correcciones

--- mochilaBPSO.py
+++ mochilaBPSO.py
@@ -8,7 +8,7 @@ def matBinAleat(orden):
     for i in range (0, orden):
         for j in range (0, orden):
             matriz[i][j] = random.randint(0, 1)
-    print(matriz)
+    # print(matriz)
     return matriz
 
 # Genera una matriz cuadrada (según el orden) con valores reales en un
@@ -19,7 +19,7 @@ def matRealAleat(orden):
         for j in range (0, orden):
             # Números aleatoriamente distribuidos uniformemente entre -4 y 4 y redondeados a dos decimales
             matriz[i][j] = round(random.uniform(-4, 4), 2)
-    print(matriz)
+    # print(matriz)
     return matriz
 
 
@@ -41,7 +41,14 @@ def fitness(arr, prtc):
     #     '''.format(selec, arr[idx]['profit'], 'yes' if selec == 1 else 'no'))
     # print('Suma: {}'.format(suma))
     return suma
-
+# Devuelve la relación de pesos respecto al fitness. Lo mismo que el fitness, pero con pesos
+def fitnessPesos(arr, prtc):
+    suma = 0
+    for idx, selec in enumerate(prtc): # selec es un número binario de la partícula
+        # print('{} * {} = {}'.format(selec, arr[idx]['weight'], selec * arr[idx]['weight']))
+        suma += selec * arr[idx]['weight']
+    # print('SUM: {}'.format(suma))
+    return suma
 
 if __name__ == "__main__":
     
@@ -74,44 +81,80 @@ if __name__ == "__main__":
     # Inicialización de las matrices
     xArr = matBinAleat(len(arr))
     pArr = xArr
-    print(pArr)
+    # print(pArr)
     vArr = matRealAleat(len(arr))
 
     #Esta parte hay que agregarla a una función para poder retornar pArr[g] y fitness(pArr[g])
     # Además, los comentarios de 'Repite' y 'Hasta que se alcance la condición de paro' están porque no implementé dicha condición (hay que hacerlo (debe ser una clase de ciclo o asi, creo))
 
-    # Repite
+    fitnessPgMax = 0 # Fitness máximo
+    pesosPgMax   = 0 # Peso máximo en relación al fitness
+    cntMax       = 0 # Cuenta la cantidad de veces que se ha obtenido un máximo
     
-    # Recorre todas las partículas en arr y extrae el índice i
-    for i, prtc in enumerate(arr):
-        # Comparación de fitness en las dimensiones de X y P
-        xFitAct = fitness(arr, xArr[i]) # Fitness de la partícula actual en matriz x
-        pFitAct = fitness(arr, pArr[i]) # Fitness de la partícula actual en matriz p
-        if xFitAct > pFitAct:
-            # Si el fitness en x es mayor que en p, cambia los elementos de X a P
-            for d, elem in enumerate(xArr):
-                pArr[i][d] = xArr[i][d] # Actualización de la matriz p
+    contador     = 0 # Cantidad de recorridos para cada partícula
 
-        g = i # Almacena en g, el índice actual i (partícula líder actual)
-        # Para cada partícula
-        for j, elem in enumerate(arr):
-            # Si el fitness de Pj es mejor que el de Pg, almacena en g, el índice j (partícula líder definitiva)
-            pActFit = fitness(arr, pArr[j]) # Fitness de la partícula actual en matriz p
-            pFitLider = fitness(arr, pArr[g]) # Fitness de la partícula líder marcada en matriz p
-            if pActFit > pFitLider:
-                g = j
-        # Números aleatoriamente distribuidos uniformemente entre 0 y 1 y redondeados a dos decimales
-        r1 = round(random.uniform(0, 1), 2)
-        r2 = round(random.uniform(0, 1), 2)
-        # Para cada dimensión
-        for d, elem in enumerate(xArr):
-            vArr[i][d] = round(w * vArr[i][d] + c1 * r1 * (pArr[i][d] - xArr[i][d]) + c2 * r2 * (pArr[g][d] - xArr[i][d]), 2)
-            # Cambiar Xid a 1 si sigmoide de Vid es mayor; si el random es mayor, Xid será 0
-            xRandom = round(random.uniform(0, 1), 2)
-            xSigmoide = sigmoide(vArr[i][d])
-            if xRandom < xSigmoide: 
-                xArr[i][d] = 1 
-            else: 
-                xArr[i][d] = 0
+    repetir      = True # Bandera de repetición
+    
+    while repetir:
+        contador += 1
+    # Recorre todas las partículas en arr y extrae el índice i
+        for i, prtc in enumerate(xArr):
+            # Comparación de fitness en las dimensiones de X y P
+            xFitAct = fitness(arr, xArr[i]) # Fitness de la partícula actual en matriz x
+            pFitAct = fitness(arr, pArr[i]) # Fitness de la partícula actual en matriz p
+            if xFitAct > pFitAct:
+                # Si el fitness en x es mayor que en p, cambia los elementos de X a P
+                for d, elem in enumerate(xArr[i]):
+                    pArr[i][d] = xArr[i][d] # Actualización de la matriz p
+
+            g = i # Almacena en g, el índice actual i (partícula líder actual)
+            # Para cada partícula
+            for j, elem in enumerate(pArr):
+                # Si el fitness de Pj es mejor que el de Pg, almacena en g, el índice j (partícula líder definitiva)
+                pActFit = fitness(arr, pArr[j]) # Fitness de la partícula actual en matriz p
+                pFitLider = fitness(arr, pArr[g]) # Fitness de la partícula líder marcada en matriz p
+                if pActFit > pFitLider:
+                    g = j
+            # Números aleatoriamente distribuidos uniformemente entre 0 y 1 y redondeados a dos decimales
+            r1 = round(random.uniform(0, 1), 2)
+            r2 = round(random.uniform(0, 1), 2)
+            # Para cada dimensión
+            for d, elem in enumerate(xArr):
+                vArr[i][d] = round(w * vArr[i][d] + c1 * r1 * (pArr[i][d] - xArr[i][d]) + c2 * r2 * (pArr[g][d] - xArr[i][d]), 2)
+                # Cambiar Xid a 1 si sigmoide de Vid es mayor; si el random es mayor, Xid será 0
+                xRandom = round(random.uniform(0, 1), 2)
+                xSigmoide = sigmoide(vArr[i][d])
+                if xRandom < xSigmoide: 
+                    xArr[i][d] = 1 
+                else: 
+                    xArr[i][d] = 0
+            
+            #CONDICIÓN DE PARO
+
+            fitnessPg = fitness(arr, pArr[g]) # Almacena el fitness(Pg)
+            pesosPg = fitnessPesos(arr, pArr[g]) # Almacena el la relación de peso con  fitness(Pg)
+            
+            # Obtendrá el fitnes máximo de la iteración, así como el peso y partículas asociadas
+            if (fitnessPg >= fpgMax) and (165 >= pesosPg):
+                fpgMax = fitnessPg
+                fpgWeightMax = pesosPg
+
+                maximos = {
+                    'peso'        : fitnessPg,
+                    'ganancia'    : pesosPg, 
+                    'particula'   : pArr[g],
+                    'repeticiones': contador
+                }
+                # Si en la iteración obtuvo el mismo resultado que en la iteración que proporcionó el máximo peso, aumenta el contador. En caso contrario, el contador se mantendrá en 0
+                if pesosPg == fpgWeightMax:
+                    cntMax += 1
+                    
+            if cntMax > len(arr):
+                repetir = False
+
+    print('peso: {}'.format(maximos['peso']))
+    print('ganancia: {}'.format(maximos['ganancia']))
+    print('partícula: {}'.format(maximos['particula']))
+    print('repeticiones: {}'.format(maximos['repeticiones']))
 
     # Hasta que se alcance la condición de paro
